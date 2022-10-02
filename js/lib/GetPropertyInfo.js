6ad74d52a4dd05b7c5ad435039615eb937b5b979
sysionng <sysionng@gmail.com>
2022-10-02 23:34:57 +0100

    Updated API url to remote ip-address

--- js/lib/GetPropertyInfo.js
+++ js/lib/GetPropertyInfo.js
@@ -6,17 +6,12 @@ class GetPropertyInfo{
 	}
 
 	async getAllProperties(){
-		const response = await fetch('http://127.0.0.1:12345/api/houses/index.php');						// home laptop
-		//const response = await fetch('http://127.0.0.1:12345/index.php');									// office laptop
-		//const response = await fetch('https://houserestapi.herokuapp.com/index.php'); 					// remote
+		const response = await fetch('https://houserestapi.herokuapp.com/index.php'); 					// remote
     	return response.json();
 	}
 
 	async getProperty(id){
-		const response = await fetch(`http://127.0.0.1:12345/api/houses/index.php?id=${id}`);				// home laptop
-		//const response = await fetch(`http://127.0.0.1:12345/index.php?house=${id}`);						// office laptop
-		//const response = await fetch(`https://houserestapi.herokuapp.com/index.php?id=${id}`);			// remote - nok
-		//const response = await fetch(`https://houserestapi.herokuapp.com/index.php?house=${id}`);			// remote
+		const response = await fetch(`https://houserestapi.herokuapp.com/index.php?house=${id}`);			// remote
 		return response.json();
 	}
 }
